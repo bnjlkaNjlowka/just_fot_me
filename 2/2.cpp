771b005224929454bddfabecb358c166e3feb2a9
bnjlkaNjlowka <dolwans12@gmail.com>
2022-07-18 14:15:53 +0300

    2

---
+++ 2/2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iterator>
+#include <ostream>
+
+int algoritm_euklid(int num1,int num2) {
+	if (num1 > num2) {
+		//std::cout << num1 << " " << num2 << std::endl;
+		while (num1 !=0 && num2 !=0) {
+			//std::cout << a << std::endl;
+			//std::cout << "num1 " << num1 << "num2 " << num2 << std::endl;
+			while (num1 >= num2 && num2 != 0) {
+				num1 = num1 - num2;
+				//std::cout << "num1 " << num1 << std::endl;
+			}
+			while (num2 >= num1 && num1 != 0) {
+				num2 = num2 - num1;
+				//std::cout << "num2 " << num2 << std::endl;
+			}
+		}
+	}
+	else
+		num1=algoritm_euklid(num2, num1);
+
+	if (num1 == 0)
+		return num2;
+	else
+		return num1;
+}
+
+int main() {
+	int num1, num2;
+	std::cout << "Введите число 1:";
+	std::cin >> num1;
+	std::cout << std::endl;
+	std::cout << "Введите число 2:";
+	std::cin >> num2;
+	std::cout << std::endl;
+	int ans=algoritm_euklid(num1, num2);
+	std::cout << "Наибольший общий делитель введенных чисел:" << ans << std::endl; 
+	return 0;
+}
+
+
+
+
+
+
