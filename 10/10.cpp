f56baa9fa9fac0e3a9c9d247572c1ac35351cf40
bnjlkanjlowka <dolwans12@gmail.com>
2022-10-18 18:51:42 +0300

    10

--- 10/10.cpp
+++ 10/10.cpp
@@ -1,23 +1,29 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 
-int code_to_gray(int num) {
-	return num ^ (num >> 1);
-}
-
-int gray_decode(int num) {
-	int temp_num = num;
-	while (num != 0) {
-		temp_num = temp_num ^ (num >> 1);
-	}
-	return temp_num;
-}
-
 std::vector <int> iz_10_v_2(int num) {
-	std::vector ans;
+	std::vector <int> ans;
+	int ost = 0, n = 0;
 	while (num != 1) {
-		ans = 
+		ost = num % 2;
+		num = num / 2;
+		std::cout << ost << std::endl;
+		ans.push_back(ost);
+		n = n + 1;
+	}
+	ans.push_back(1);
+	return ans;
+}
 
-
-
-
+int main() {
+	std::vector <int> ans;
+	int num;
+	std::cout << "Введите число: ";
+	std::cin >> num;
+	std::cout << std::endl;	
+	ans = iz_10_v_2(num);
+	for (int i = ans.size() - 1; i != -1; i--)
+		std::cout << ans[i];
+	std::cout << std::endl;
+	return 0;
+}
